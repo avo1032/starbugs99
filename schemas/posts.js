a38f119882000a204e7c912cd6c2afc835af54ac
seoa909 <seoa909@gmail.com>
2022-06-11 05:11:55 -0500

    added schema

---
+++ schemas/posts.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const PostsSchema = new mongoose.Schema({
+      postId: {
+        type: String,
+        required: true,
+      }, 
+      nickname: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true, 
+      },
+      content: {
+        type: String,
+        required: true
+      },    
+      date: {
+        type: Date,
+        default: Date.now(),
+        
+      },
+      imageUrl: { 
+        type: String,
+        required: true
+      },
+      likeCnt: {
+        type: Number,
+        required: true,
+      },
+      commentCnt: {
+        type: Number,
+        required: true,
+      },
+      userLike: {
+        type: Array,
+        required: true,
+      }
+    });
+
+    module.exports = mongoose.model("Posts", PostsSchema,);
