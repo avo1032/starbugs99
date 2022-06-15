be6af7ed515ff3b3b413dabfa13aedd99ff78a1b
avo1032 <swan1032@naver.com>
2022-06-15 17:22:45 +0900

    코밋!

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,21 @@ const express = require("express");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const User = require("../schemas/users");
+const authMiddleware = require("../middlewares/auth-middleware");
+const { Router } = require("express");
 const router = express.Router();
 
-const postUsersSchema = Joi.object({
+/*const postUsersSchema = Joi.object({
+    // userId: 4~12글자, 알파벳 대소문자, 숫자 가능
+    userId: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{4,12}$")).required(),
+    // nickname: 2~16글자, 알파벳 대소문자, 숫자, 한글 가능
+    nickname: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]{2,16}$"))
+      .required(),
+    password: Joi.string().min(6).max(18).required(),
+  });*/
+
+  const postUsersSchema = Joi.object({
     userId: Joi
         .string()
         .required(),
@@ -16,10 +28,8 @@ const postUsersSchema = Joi.object({
     password: Joi
         .string()
         .required()
-});
-
-
-
+  });
+  
 //회원가입
 router.post("/register", async (req, res) => {
     try {
@@ -28,12 +38,9 @@ router.post("/register", async (req, res) => {
         nickname,
         password,
       } = await postUsersSchema.validateAsync(req.body);
-  
+
       const existUsers = await User.find({
-        userId
-      });
-      const existNick = await User.find({
-        nickname
+        $or: [{ userId }, { nickname }],
       });
       if (existUsers.length) {
         res.status(400).send({
@@ -41,16 +48,9 @@ router.post("/register", async (req, res) => {
         });
         return;
       }
-      if (existNick.length) {
-        res.status(400).send({
-          errorMessage: "이미 가입된 이메일 또는 닉네임이 있습니다.",
-        });
-        return;
-      }
-  
+
       const user = new User({ userId, nickname, password });
       await user.save();
-  
       res.status(201).send({ message: "회원가입이 완료!" });
     } catch (err) {
       console.log(err);
@@ -60,29 +60,30 @@ router.post("/register", async (req, res) => {
     }
   });
 
+
 //로그인
   const postAuthSchema = Joi.object({
-    userId: Joi.string().required(),
+    userId: Joi.string().min(3).max(12).required(),
     password: Joi.string().required(),
   });
+  
+  
   router.post("/login", async (req, res) => {
     try {
-      console.log(userId, password);
-      const { userId, password } = await req.body;
-  
+    //  const { userId, password } = await postAuthSchema.validateAsync(req.body);
+      const { userId, password } = await (req.body);
       const user = await User.findOne({ userId, password }).exec();
-  
       if (!user) {
         res.status(400).send({
           errorMessage: "이메일 또는 패스워드가 잘못됐습니다.",
         });
         return;
       }
-
       const token = jwt.sign({ userId: user.userId }, "my-secret-key");
       res.send({
         token,
       });
+
     } catch (err) {
         console.log(err);
       res.status(400).send({
